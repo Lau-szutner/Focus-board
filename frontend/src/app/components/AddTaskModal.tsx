d54b0694c25f82fdba53e07bf910d50168b8fbbf
Lautaro Fernandez Szutner <111034147+Lau-szutner@users.noreply.github.com>
2025-07-25 05:43:15 +0000

    agrego modal newtask

--- frontend/src/app/components/AddTaskModal.tsx
+++ frontend/src/app/components/AddTaskModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
   isOpen: boolean;
@@ -9,21 +9,48 @@ const AddTaskModal: React.FC<Props> = ({ isOpen, toggleModal }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="bg-white border p-4 rounded shadow-md">
-      <button onClick={toggleModal} className="mb-4 text-red-500">
-        Close
-      </button>
-      <form className="bg-blue-500 p-4 rounded">
-        <label htmlFor="title" className="block text-white mb-2">
-          Title
-        </label>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          className="w-full p-2 rounded"
-        />
-      </form>
+    <div className="fixed inset-0 bg-neutral-200/70 flex items-center justify-center z-50">
+      <div className="bg-white grid gap-4 p-4 rounded-md place-items-center justify-evenly">
+        <h2 className="text-2xl">New task</h2>
+        <form className="p-4 rounded grid gap-4">
+          <label htmlFor="title" className="">
+            Title
+            <input
+              type="text"
+              name="title"
+              id="title"
+              className="w-full p-2 rounded border border-neutral-300"
+              placeholder="Your task title"
+            />
+          </label>
+          <label htmlFor="title" className="">
+            Description
+            <input
+              type="text"
+              name="body"
+              id="body"
+              className="w-full p-2 rounded border border-neutral-300"
+              placeholder="Your task description"
+            />
+          </label>
+          <label htmlFor="title" className="">
+            Hours
+            <input
+              type="text"
+              name="title"
+              id="title"
+              className="w-full p-2 rounded border border-neutral-300"
+              placeholder="Your task title"
+            />
+          </label>
+        </form>
+        <button
+          onClick={toggleModal}
+          className="bg-red-500 font-bold w-8/12 py-2 rounded-md"
+        >
+          Close
+        </button>
+      </div>
     </div>
   );
 };
--- frontend/src/app/components/Board.tsx
+++ frontend/src/app/components/Board.tsx
@@ -7,21 +7,43 @@ import EventsColumn from '@/app/components/EventsColumn';
 import NewTaskBtn from '@/app/components/NewTaskBtn';
 
 const Board: React.FC = () => {
-  const [tasks, setTasks] = useState<any[]>([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://127.0.0.1:3000/tasks');
-        const data = await response.json();
-        setTasks(data);
-        console.log(data);
-      } catch (error) {
-        console.error('Error al obtener las tareas:', error);
-      }
-    };
+  // const [tasks, setTasks] = useState<any[]>([]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch('http://127.0.0.1:3000/tasks');
+  //       const data = await response.json();
+  //       setTasks(data);
+  //       console.log(data);
+  //     } catch (error) {
+  //       console.error('Error al obtener las tareas:', error);
+  //     }
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
+
+  //Usar en caso de trabajar en Codespaces
+  const tasks = [
+    {
+      title: 'Tarea num1',
+      body: 'Esta es la tarea numero 1',
+      startTime: '7',
+      endTime: '9',
+    },
+    {
+      title: 'Tarea num2',
+      body: 'Esta es la tarea numero 2',
+      startTime: '7',
+      endTime: '9',
+    },
+    {
+      title: 'Tarea num3',
+      body: 'Esta es la tarea numero 3',
+      startTime: '7',
+      endTime: '9',
+    },
+  ];
 
   return (
     <section className="w-11/12 mx-auto p-4 h-full">
